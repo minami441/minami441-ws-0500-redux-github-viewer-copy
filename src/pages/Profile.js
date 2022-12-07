7589be758b71b81c093797227bca11aace7a4291
minami441 <minami1195@outlook.jp>
2022-12-07 23:02:46 +0900

    update:コピー用に作成

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -43,25 +43,7 @@ const ProfileText = styled.div`
 
 const ProfileBlock = styled.div``;
 
-const urlProfile = "https://api.github.com/user";
-
 function Profile() {
-  const [user, setUser] = React.useState("");
-  function getProfile() {
-    axios({
-      method: "GET",
-      url: urlProfile,
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_GITAUTH}`,
-      },
-    }).then((response) => {
-      // handle succes
-      setUser(response.data);
-    });
-  }
-  useEffect(() => {
-    getProfile();
-  }, []);
   return (
     <Profiler>
       <ProfileTitle>Profile</ProfileTitle>
